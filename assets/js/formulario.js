e9993b66e3baed19c0a6523c4392c5eedc811a5b
AlexandreKc <sebastiancarrera2021@gmail.com>
2024-05-20 15:44:23 -0400

    envio de correo incompleto

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,9 +1,10 @@
 
 document.addEventListener('DOMContentLoaded', function() {
+    /*Validacion de formulario */
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{8,45}$/, // Letras y espacios, pueden llevar acentos.
     nota: /^[a-zA-ZÀ-ÿ\s]{1,160}$/, // Letras y espacios, pueden llevar acentos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
 const campos={
     nombre:false,
@@ -11,9 +12,10 @@ const campos={
     correo:false
 }
 
+
 const validarFormulario = (e) =>{
     switch (e.target.name){
-        case "nombre":
+        case "from_name":
             if(expresiones.nombre.test(e.target.value)){
                 campos.nombre=true;
                 document.getElementById('nombreERROR').innerHTML = '';
@@ -21,16 +23,16 @@ const validarFormulario = (e) =>{
                 document.getElementById('nombreERROR').innerHTML = '<p class="error">El nombre debe tener entre 8 y 45 caracteres.</p>';
             }
         break;
-        case "nota":
-            if(expresiones.nombre.test(e.target.value)){
+        case "message":
+            if(expresiones.nota.test(e.target.value)){
                 campos.nota=true;
                 document.getElementById('notaERROR').innerHTML = '';
             }   else {
                 document.getElementById('notaERROR').innerHTML = '<p class="error">Debe ingresar texto.</p>';
             }
         break;
-        case "correo":
-            if(expresiones.nombre.test(e.target.value)){
+        case "email_id":
+            if(expresiones.correo.test(e.target.value)){
                 campos.correo=true;
                 document.getElementById('correoERROR').innerHTML = '';
             }   else {
@@ -46,18 +48,38 @@ inputs.forEach((input) => {
     input.addEventListener('keyup',validarFormulario);
     input.addEventListener('blur',validarFormulario);
 });
-
+const btn = document.getElementById('button');
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
     if (campos.nombre && campos.nota && campos.correo){
-        console.log(e.target.nombre.value)
-        console.log(e.target.nota.value)
-        console.log(e.target.correo.value)
-        myForm.reset();
+        console.log(e.target.from_name.value)
+        console.log(e.target.message.value)
+        console.log(e.target.email_id.value)
         document.getElementById('info_valida').innerHTML = '<p class="error">Información enviada.</p>';
     } else {
         document.getElementById('info_invalida').innerHTML = '<p class="error">Campos inválidos.</p>';
     }
+});
+
+/*Aqui esta toda la configuración para enviar formulario */
+
+document.getElementById('myForm')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_gvj7809';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
 });
 
 
